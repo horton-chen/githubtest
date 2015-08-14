c7bf79f5ec7a55e57beb7d859e10609abddb3e2f
horton_chen <firetim@126.com>
2015-08-14 11:11:29 +0800

    Version 4

--- app/src/main/java/com/example/horton/githubtest/MainActivity.java
+++ app/src/main/java/com/example/horton/githubtest/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    /** This is version 3 */
+    /** This is version 4 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
